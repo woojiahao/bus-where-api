8d56d107a80b106e2cca194d7a66974445ef1c75
Jiahao, Woo <woojiahao1234@gmail.com>
2026-06-05 23:28:58 +0800

    Add model base for ensuring parsing consistency

---
+++ lib/bus_where_api/models/model_base.ex
@@ -0,0 +1,9 @@
+defmodule BusWhereApi.Models.ModelBase do
+  @callback from_body(map()) :: struct()
+
+  defmacro __using__(_opts) do
+    quote do
+      @behaviour BusWhereApi.Models.ModelBase
+    end
+  end
+end
