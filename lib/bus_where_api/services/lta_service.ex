b443adaf000b7440df9a10072671648a3cf805a2
Jiahao, Woo <woojiahao1234@gmail.com>
2026-06-06 11:02:05 +0800

    Add service layer support for BusServices

--- lib/bus_where_api/services/lta_service.ex
+++ lib/bus_where_api/services/lta_service.ex
@@ -1,20 +1,28 @@
 defmodule BusWhereApi.Services.LtaService do
+  alias BusWhereApi.Models
+
   @cache_ttl_minutes 60
 
-  @spec arrival(integer(), String.t() | nil) ::
-          list(BusWhereApi.Models.BusArrival) | {:error, BusWhereApi.Error.t()}
-  def arrival(bus_stop_code, service_no \\ nil) do
-    base_parameters = %{"BusStopCode" => bus_stop_code}
-
-    parameters =
-      if is_nil(service_no),
-        do: base_parameters,
-        else: Map.put(base_parameters, "ServiceNo", service_no)
-
-    case cache_fetch("v3/BusArrival", parameters, 20) do
+  @spec bus_arrival(integer(), String.t() | nil) ::
+          list(Models.BusArrival.t()) | {:error, BusWhereApi.Error.t()}
+  def bus_arrival(bus_stop_code, service_no \\ nil) do
+    case cache_fetch(
+           "v3/BusArrival",
+           %{"BusStopCode" => bus_stop_code, "ServiceNo" => service_no},
+           20
+         ) do
       {:error, err} -> {:error, err}
-      %{"Services" => arrivals} -> Enum.map(arrivals, &BusWhereApi.Models.BusArrival.from_body/1)
+      %{"Services" => arrivals} -> Enum.map(arrivals, &Models.BusArrival.from_body/1)
       _ -> {:error, BusWhereApi.Error.not_found("Arrival information could not be found")}
+    end
+  end
+
+  @spec bus_services(String.t() | nil) ::
+          list(Models.BusService.t()) | {:error, BusWhereApi.Error.t()}
+  def bus_services(service_no \\ nil) do
+    case cache_fetch_all("BusServices", "value", %{"ServiceNo" => service_no}, 60) do
+      {:error, err} -> {:error, err}
+      services -> Enum.map(services, &Models.BusService.from_body/1)
     end
   end
 
@@ -42,8 +50,7 @@ defmodule BusWhereApi.Services.LtaService do
         models
 
       {:ok, false} ->
-        case BusWhereApi.Clients.LtaClient.get_all(path, path_parameters, list_key)
-             |> IO.inspect() do
+        case BusWhereApi.Clients.LtaClient.get_all(path, path_parameters, list_key) do
           {:error, err} ->
             {:error, BusWhereApi.Error.external_failure(err)}
 
@@ -75,11 +82,9 @@ defmodule BusWhereApi.Services.LtaService do
 
       {:ok, false} ->
         full_path =
-          BusWhereApi.Clients.LtaClient.create_path(path, path_parameters |> IO.inspect())
-          |> IO.inspect()
+          BusWhereApi.Clients.LtaClient.create_path(path, path_parameters)
 
-        case BusWhereApi.Clients.LtaClient.get(full_path)
-             |> IO.inspect() do
+        case BusWhereApi.Clients.LtaClient.get(full_path) do
           {:error, err} ->
             {:error, BusWhereApi.Error.external_failure(err)}
 
