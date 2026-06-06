e24fa4c47c9e9e8e2891c13d76638b53d2a70869
Jiahao, Woo <woojiahao1234@gmail.com>
2026-06-06 11:02:13 +0800

    Add BusServices endpoint for LTAController

--- lib/bus_where_api_web/controllers/lta_controller.ex
+++ lib/bus_where_api_web/controllers/lta_controller.ex
@@ -3,22 +3,29 @@ defmodule BusWhereApiWeb.LtaController do
 
   action_fallback BusWhereApiWeb.Controllers.FallbackController
 
-  def arrival(conn, %{"bus_stop_code" => bus_stop_code, "service_no" => service_no}) do
-    case BusWhereApi.Services.LtaService.arrival(bus_stop_code, service_no) do
+  def bus_arrival(conn, %{"bus_stop_code" => bus_stop_code, "service_no" => service_no}) do
+    case BusWhereApi.Services.LtaService.bus_arrival(bus_stop_code, service_no) do
       {:error, err} -> {:error, err}
       arrival -> json(conn, arrival)
     end
   end
 
-  def arrival(conn, %{"bus_stop_code" => bus_stop_code}) do
-    case BusWhereApi.Services.LtaService.arrival(bus_stop_code) do
+  def bus_arrival(conn, %{"bus_stop_code" => bus_stop_code}) do
+    case BusWhereApi.Services.LtaService.bus_arrival(bus_stop_code) do
       {:error, err} -> {:error, err}
       arrival -> json(conn, arrival)
     end
   end
 
-  def arrival(_, _) do
+  def bus_arrival(_, _) do
     {:error, BusWhereApi.Error.bad_request("Missing bus_stop_code parameter")}
+  end
+
+  def bus_services(conn, params) do
+    case BusWhereApi.Services.LtaService.bus_services(Map.get(params, "service_no")) do
+      {:error, err} -> {:error, err}
+      services -> json(conn, services)
+    end
   end
 
   def bus_routes(conn, _params) do
