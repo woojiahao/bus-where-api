b942b35286ceee97f7b04f068457980d58453363
Jiahao, Woo <woojiahao1234@gmail.com>
2026-06-06 11:21:21 +0800

    Implement proper BusStop data model

--- lib/bus_where_api/models/bus_stop.ex
+++ lib/bus_where_api/models/bus_stop.ex
@@ -1,6 +1,9 @@
 defmodule BusWhereApi.Models.BusStop do
   use BusWhereApi.Models.ModelBase
 
-  field(:code, :integer, "BusStopCode", default: "0")
-  field(:service_no, :string, "ServiceNo", default: "")
+  field(:bus_stop_code, :integer, "BusStopCode", default: 0)
+  field(:road_name, :string, "RoadName", default: "")
+  field(:description, :string, "Description", default: "")
+  field(:latitude, :float, "Latitude", default: 0.0)
+  field(:longitude, :float, "Longitude", default: 0.0)
 end
